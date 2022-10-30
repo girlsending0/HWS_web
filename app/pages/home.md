3fe84b7c84f51dfb3fe6840b2e9ab72ea98b236c
girlsending0 <girlsending0@khu.ac.kr>
2022-10-30 19:14:39 +0900

    first commit

---
+++ app/pages/home.md
@@ -0,0 +1,10 @@
+<div id="top">
+<h1>Welcome to HJ WJ SH project - Transfer Hair</h1>
+<p></p>
+<p>
+This is the HWJ project space.
+</p>
+<p>
+It is a service that change images and styles with the hair Transfer model.
+</p>
+</div>
---
+++ app/routers/index.py
@@ -0,0 +1,70 @@
+from fastapi import Request, APIRouter, UploadFile, File
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+# from app.library.helpers import * --> 모델 관련된 부분으로 추정됨
+# from app.library.style_transfer import save_style_transfer_image
+
+import shutil
+from pydantic.main import BaseModel
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates/")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = File("home.md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+
+
+
+
+
+
+'''
+class UploadedData(BaseModel):
+    img_path: str
+    thumb_path: str
+
+
+@router.post("/upload/new/", response_model=UploadedData)
+async def post_upload(imgdata: tuple, file: UploadFile = File(...)):
+    data_dict = eval(imgdata[0])
+    winWidth, imgWidth, imgHeight = data_dict["winWidth"], data_dict["imgWidth"], data_dict["imgHeight"]
+
+    # create the full path
+    workspace = create_workspace()
+    # filename
+    file_path = Path(file.filename)
+    # image full path
+    img_full_path = workspace / file_path
+    with open(str(img_full_path), 'wb') as myfile:
+        contents = await file.read()
+        myfile.write(contents)
+    # create a thumb image and save it
+    thumb(img_full_path, winWidth, imgWidth, imgHeight)
+    # create the thumb path
+    # ext is like .png or .jpg
+    filepath, ext = os.path.splitext(img_full_path)
+    thumb_path = filepath + ".thumbnail"+ext
+
+    return UploadedData(
+        img_path=str(img_full_path),
+        thumb_path=str(thumb_path)
+    )
+
+
+class TransferData(BaseModel):
+    result: str
+
+
+@router.get("/style_transfer", response_model=TransferData)
+async def style_transfer(original_path: str, style_path: str):
+    transfer_path = save_style_transfer_image(content_path=original_path, style_path=style_path)
+    shutil.rmtree(original_path.replace(original_path.split('/')[-1], ''))
+    shutil.rmtree(style_path.replace(style_path.split('/')[-1], ''))
+
+    return TransferData(result=str(transfer_path))
+'''
